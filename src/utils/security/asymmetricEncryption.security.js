980dc453adc092afd8b1a0439ba769968b6e650a
Ahmedsayed322 <mamdatap@gmail.com>
2026-02-14 10:08:49 +0200

    upadate-saraha

--- src/utils/security/asymmetricEncryption.security.js
+++ src/utils/security/asymmetricEncryption.security.js
@@ -1,61 +1,99 @@
+import { dbService, OTP, USER } from '../../DB/index.js';
 import {
-  generateKeyPairSync,
-  constants,
-  publicEncrypt,
-  privateDecrypt,
-} from 'crypto';
-import { resolve } from 'path';
-import fs from 'fs/promises';
-export const runForFirstTime = async () => {
-  const { publicKey, privateKey } = generateKeyPairSync('rsa', {
-    modulusLength: 2048,
-  });
-  try {
-    await fs.mkdir(resolve('./src/config/security'), { recursive: true });
-    await fs.writeFile(
-      resolve('./src/config/security/public.bem'),
-      publicKey.export({ type: 'pkcs1', format: 'pem' }),
-      { flag: 'wx' },
-    );
-    await fs.writeFile(
-      resolve('./src/config/security/private.bem'),
-      privateKey.export({ type: 'pkcs1', format: 'pem' }),
-      { flag: 'wx' },
-    );
-    console.log('files created successfully');
-  } catch (e) {
-    if (e.code === 'EEXIST') {
-      console.log('files already exists skipping....');
-    }
+  ApiError,
+  asymmetric,
+  hashSecurity,
+  mailService,
+  otpTypes,
+  pages,
+} from '../../utils/index.js';
+
+export const signup = async (data) => {
+  const { firstName, lastName, email, password, cPassword, phone } = data;
+
+  const exists = await dbService.findOneDoc(USER, { filter: { email } });
+  console.log(email, exists);
+
+  if (exists) {
+    throw new ApiError('this email already exists', 409);
   }
-};
-////////////////////////////
-const publicKey = await fs.readFile(
-  resolve('./src/config/security/public.bem'),
-);
-const privateKey = await fs.readFile(
-  resolve('./src/config/security/private.bem'),
-);
-/////////////////////////////////////////
-export const Encryption = async (data) => {
-  const encrypted = publicEncrypt(
-    {
-      key: publicKey,
-      padding: constants.RSA_PKCS1_OAEP_PADDING,
+  if (cPassword !== password) {
+    throw new ApiError("confirm password doesn't match password", 400);
+  }
+  const expireTime = new Date(Date.now() + 10 * 60 * 1000);
+
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  console.log('otp : ', otp);
+  //el hash by7sl f el pre save w el encryption by7sl f el phone f el pre save
+  const user = await dbService.createDoc(USER, {
+    data: {
+      firstName,
+      lastName,
+      email,
+      password,
+      phone,
+      expireAt: expireTime,
     },
-    Buffer.from(data),
-  );
-  return encrypted.toString('base64');
-};
-//////////////////////////////////////////////
-export const Decryption = async (encryptedData) => {
-  const buffer = Buffer.from(encryptedData, 'base64');
-  const decrypted = privateDecrypt(
-    {
-      key: privateKey,
-      padding: constants.RSA_PKCS1_OAEP_PADDING,
+  });
+  await dbService.createDoc(OTP, {
+    data: {
+      otpType: otpTypes.confirmEmail,
+      otp,
+      userId: user._id,
+      expireAt: expireTime,
     },
-    buffer,
+  });
+  await mailService.sendOTP(
+    email,
+    'Confirm Your Email',
+    'Your OTP',
+    pages.otpPage(otp),
   );
-  return decrypted.toString('utf8')
+};
+export const confirmAccount = async (data) => {
+  const { email, otp } = data;
+  const user = await dbService.findOneDoc(USER, { filter: { email } });
+  if (!user) {
+    throw new ApiError('email not found', 404);
+  }
+  const otpDoc = await dbService.findOneDoc(OTP, {
+    filter: { userId: user._id, otpType: otpTypes.confirmEmail },
+  });
+  if (!otpDoc) {
+    throw new ApiError('invalid OTP ', 400);
+  }
+  const decryptedOTP = await hashSecurity.checkHash(otp, otpDoc.otp, 'bcrypt');
+  if (!decryptedOTP) {
+    throw new ApiError('invalid OTP ', 400);
+  }
+  await dbService.deleteOneDoc(OTP, { filter: { _id: otpDoc._id } });
+  await dbService.updateOneDoc(USER, {
+    filter: { _id: user._id },
+    update: {
+      isConfirmed: true,
+      $unset: { expireAt: '' },
+    },
+  });
+};
+export const login = async (data) => {
+  const { email, password } = data;
+  const user = await dbService.findOneDoc(USER, { filter: { email } });
+
+  if (!user) {
+    throw new ApiError('invalid email or password', 401);
+  }
+  if (!user.isConfirmed) {
+    throw new ApiError('please confirm your email first', 403);
+  }
+  const checkPassword = await hashSecurity.checkHash(
+    password,
+    user.password,
+    'argon',
+  );
+
+  if (!checkPassword) {
+    throw new ApiError('invalid email or password', 401);
+  }
+  const token = await user.generateToken();
+  return token;
 };
